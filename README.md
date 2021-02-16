2c85f8e181a09fddc49cba38f8c8cb31b6b8df75
Peter Volf <do.volfp@gmail.com>
2021-02-16 10:36:03 +0100

    added great_circle_distance + config + description

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
-# great-circle-distance
+[![Code style: black](https://img.shields.io/badge/code%20style-black-000000.svg)](https://github.com/psf/black)
+[![Checked with mypy](http://www.mypy-lang.org/static/mypy_badge.svg)](http://mypy-lang.org/)
+
+# Great circle distance
+
 Simple great circle distance calculation package
+
+## Usage
+
+```python
+from great_circle_distance import PlanetRadius, great_circle_distance
+
+stonehenge = (51.178939, -1.825941)
+great_pyramid_of_giza = (29.979123, 31.134234)
+
+print(
+    "Distance between Stonehenge and the Great Pyramid of Giza is "
+    f"{great_circle_distance(stonehenge, great_pyramid_of_giza)} kilometers or "
+    f"{great_circle_distance(stonehenge, great_pyramid_of_giza, radius=PlanetRadius.earth_mi)} miles."
+)
+```
+
+## License - MIT
+
+The library is open-sourced under the conditions of the MIT [license](https://choosealicense.com/licenses/mit/).
---
+++ great_circle_distance.py
@@ -0,0 +1,88 @@
+from typing import Tuple
+
+import math
+
+Coordinate = Tuple[float, float]
+
+
+def coordinate_to_radians(value: Coordinate) -> Coordinate:
+    """
+    Converts the given coordinate pair from degrees to radians.
+
+    Arguments:
+        value: The degree coordinate pair to convert.
+    """
+    return (math.radians(value[0]), math.radians(value[1]))
+
+
+class PlanetRadius:
+    """
+    Solar System planet radii in kilometers and miles.
+    """
+
+    mercury_km = 2439.7
+    mercury_mi = 1515.97
+
+    venus_km = 6051.8
+    venus_mi = 3760.44
+
+    earth_km = 6371.0088
+    earth_mi = 3958.8
+
+    mars_km = 3389.5
+    mars_mi = 2106.1
+
+    jupiter_km = 69911
+    jupiter_mi = 43441
+
+    saturn_km = 58232
+    saturn_mi = 36184
+
+    uranus_km = 25362
+    uranus_mi = 15759
+
+    neptune_km = 24622
+    neptune_mi = 15300
+
+
+def great_circle_distance(first: Coordinate, second: Coordinate, *, radius: float = PlanetRadius.earth_km) -> float:
+    """
+    Calculates the great circle distance between the given (latitude, longitude) coordinate pairs.
+
+    Latitude and longitude values are expected to be in *degrees*.
+
+    Arguments:
+        first: The first (latitude, longitude) pair.
+        second: The second (latitude, longitude) pair.
+        radius: Sphere radius (or mean spheroid radius), default is `PlanetRadius.earth_km`.
+
+    Returns:
+        The calculated distance.
+    """
+    return great_circle_distance_rad(
+        coordinate_to_radians(first),
+        coordinate_to_radians(second),
+        radius=radius,
+    )
+
+
+def great_circle_distance_rad(
+    first: Coordinate, second: Coordinate, *, radius: float = PlanetRadius.earth_km
+) -> float:
+    """
+    Calculates the great circle distance between the given (latitude, longitude) coordinate pairs.
+
+    Latitude and longitude values are expected to be in *radians*.
+
+    Arguments:
+        first: The first (latitude, longitude) pair.
+        second: The second (latitude, longitude) pair.
+        radius: Sphere radius (or mean spheroid radius), default is `PlanetRadius.earth_km`.
+
+    Returns:
+        The calculated distance.
+    """
+    return radius * math.acos(
+        math.sin(first[0]) * math.sin(second[0])
+        + math.cos(first[0]) * math.cos(second[0]) * math.cos(first[1] - second[1])
+    )
